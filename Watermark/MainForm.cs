4bd776535b4499993df7443606936d937e78104d
Nathan Woodburn <github@nathan.woodburn.au>
2023-06-30 16:56:36 +1000

    sln: Added installer

--- Watermark/MainForm.cs
+++ Watermark/MainForm.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.Win32;
 
 
 namespace Watermark
